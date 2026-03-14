da23931d5a3deb5f8f58cc5602bb5d12d9ab63d3
Pasa <pavlo1191@gmail.com>
2026-03-14 03:52:30 +0100

    feat: add Docker Compose for full stack development environment

--- backend/src/MyProject.API/Program.cs
+++ backend/src/MyProject.API/Program.cs
@@ -29,6 +29,12 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
